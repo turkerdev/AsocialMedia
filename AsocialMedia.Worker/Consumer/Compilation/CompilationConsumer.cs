23e48d662b1b41abf631dfd7f88985df133327a9
Serhat TURKER <oqzcmcuxr@mozmail.com>
2022-07-19 17:24:55 +0300

    compilation use file instead of pipe

--- AsocialMedia.Worker/Consumer/Compilation/CompilationConsumer.cs
+++ AsocialMedia.Worker/Consumer/Compilation/CompilationConsumer.cs
@@ -1,6 +1,5 @@
 ﻿using AsocialMedia.Worker.Services;
 using FFMpegCore;
-using FFMpegCore.Pipes;
 using Google.Apis.YouTube.v3.Data;
 
 namespace AsocialMedia.Worker.Consumer.Compilation;
@@ -20,19 +19,25 @@ internal class CompilationConsumer : IConsumer<CompilationConsumerMessage>
         {
             Console.WriteLine("{0}: Downloading {1}/{2}", directoryName, i + 1, message.Assets.Length);
             var asset = message.Assets[i];
-            var downloadStream = YTDLP.Download(asset.Url);
-            await FFMpegArguments.FromPipeInput(new StreamPipeSource(downloadStream.BaseStream))
-                .OutputToFile($"{directory}/{i}.mp4", true, opts =>
+
+            var rawPath = $"{directory}/raw_{i}.mp4";
+
+            YTDLP.Download(asset.Url, rawPath);
+            await FFMpegArguments.FromFileInput(rawPath)
+                .OutputToFile($"{directory}/ready_to_concat_{i}.mp4", true, opts =>
                 {
                     opts.WithCustomArgument(@"-filter_complex ""[0:v]boxblur=40,scale=720x1280,setsar=1[bg];[0:v]scale=720:1280:force_original_aspect_ratio=decrease[fg];[bg][fg]overlay=y=(H-h)/2""");
                 })
                 .ProcessAsynchronously();
+
+            File.Delete(rawPath);
             Console.WriteLine("{0}: Downloaded {1}/{2}", directoryName, i + 1, message.Assets.Length);
         }
 
-        var files = Directory.GetFiles(directory);
+        var allFiles = Directory.GetFiles(directory);
+        var mergeableFiles = allFiles.Where(x => x.StartsWith("ready_to_concat"));
         var outputPath = $"{directory}/output.mp4";
-        FFMpeg.Join(outputPath, files);
+        FFMpeg.Join(outputPath, mergeableFiles.ToArray());
 
         var video = new Video();
         video.Snippet = new VideoSnippet();
