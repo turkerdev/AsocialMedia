ae0c76a484b734c214734e6cbae57e13bc491c3a
Serhat TURKER <oqzcmcuxr@mozmail.com>
2022-09-07 15:45:43 +0300

    Fix arguments, add concurrency for fragments.

--- AsocialMedia.Worker/Service/YTDL/YTDLService.cs
+++ AsocialMedia.Worker/Service/YTDL/YTDLService.cs
@@ -12,6 +12,11 @@ public class YTDLService
         p.StartInfo.ArgumentList.Add(url);
         p.StartInfo.ArgumentList.Add("-f bestvideo*[height<=1080]+bestaudio/best[height<=1080]");
         p.StartInfo.ArgumentList.Add("--no-part");
+        p.StartInfo.ArgumentList.Add("--concurrent-fragments");
+        p.StartInfo.ArgumentList.Add((Math.Max(1, Process.GetCurrentProcess().Threads.Count - 1)).ToString());
+#if DEBUG
+        p.StartInfo.ArgumentList.Add("-v");
+#endif
         p.StartInfo.RedirectStandardOutput = true;
 
         p.OutputDataReceived += (_, args) =>
@@ -19,22 +24,24 @@ public class YTDLService
             if (args.Data is not null)
                 Logger.Log(args.Data);
         };
-        
+
         return p;
     }
 
     public async Task Download(string url, string outputPath, string? startTime = null, string? endTime = null)
     {
         var p = CreateProcess(url);
-        
+
         if (endTime is not null || startTime is not null)
         {
             var start = startTime ?? "0";
             var end = endTime ?? "0";
-            p.StartInfo.ArgumentList.Add($"--download-sections \"*{start}-{end}\"");
+            p.StartInfo.ArgumentList.Add("--download-sections");
+            p.StartInfo.ArgumentList.Add($"*{start}-{end}");
         }
 
-        p.StartInfo.ArgumentList.Add($"-o{outputPath}");
+        p.StartInfo.ArgumentList.Add("-o");
+        p.StartInfo.ArgumentList.Add(outputPath);
 
         p.Start();
         p.BeginOutputReadLine();
