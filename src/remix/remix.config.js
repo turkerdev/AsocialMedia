de7b136a457b787bd9557424f002ed368c14f22d
turkerdev <48413508+turkerdev@users.noreply.github.com>
2023-03-28 02:55:36 +0300

    re-make frontend

---
+++ src/remix/remix.config.js
@@ -0,0 +1,7 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  ignoredRouteFiles: ["**/.*"],
+  appDirectory: "src",
+  // assetsBuildDirectory: "public/build",
+  // publicPath: "/build/",
+};
